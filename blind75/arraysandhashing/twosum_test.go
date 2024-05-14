e8725a9a904fb22fc93b1a586f954de6e220b19e
schmeekygeek <ss6087982@gmail.com>
2024-05-14 20:35:07 +0530

    added twosum

---
+++ blind75/arraysandhashing/twosum_test.go
@@ -0,0 +1,14 @@
+package arraysandhashing
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+  got := twoSum([]int{2, 7, 11, 15}, 9)
+  expected := []int{1, 0}
+  if !reflect.DeepEqual(got, expected) {
+    t.Fatalf("TwoSum failed: expected: %v, got: %v", expected, got)
+  }
+}
