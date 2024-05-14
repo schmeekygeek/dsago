6a288df316d9d00acb003809ef49280a358184b7
schmeekygeek <ss6087982@gmail.com>
2024-05-14 20:35:03 +0530

    added containsduplicate

---
+++ blind75/arraysandhashing/containsduplicate_test.go
@@ -0,0 +1,21 @@
+package arraysandhashing
+
+import "testing"
+
+func TestContainsDuplicate(t *testing.T) {
+  nums := []int{1,1,1,3,3,4,3,2,4,2}
+  expected := true
+  got := containsDuplicate(nums)
+  if expected != got {
+    t.Fatalf("Failed ContainsDuplicate, expected: %t, got: %t", expected, got)
+  }
+}
+
+func TestContainsDuplicate2(t *testing.T) {
+  nums := []int{1,2,3,4}
+  expected := false
+  got := containsDuplicate(nums)
+  if expected != got {
+    t.Fatalf("Failed ContainsDuplicate, expected: %t, got: %t", expected, got)
+  }
+}
